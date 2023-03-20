41cbabbb022322b6dc5a7b04b39b4d81fcb7afc0
M. Rizki Aiman <rizki.aiman@arvis.id>
2023-03-20 13:58:29 +0700

    update to v13.2

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,6 @@
-import * as React from 'react'
-import '../../styles/globals.css'
+import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  const [darkMode, setDarkMode] = React.useState<boolean>(false)
-  const onToggleScreen = () => setDarkMode(!darkMode)
-
-  return (
-    <div className={`transition ease-out duration-150 bg-white ${darkMode ? 'dark' : ''}`}>
-      <Component {...pageProps} />
-    </div>
-  )
+  return <Component {...pageProps} />
 }
--- src/types/index.d.ts
+++
@@ -1,7 +0,0 @@
-import { NextRouter } from 'next/router'
-import React, { Ref } from 'react'
-import type { ReactNode } from 'react'
-
-export interface WithChildren {
-  children: React.ReactNode
-}
